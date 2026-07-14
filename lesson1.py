1503cda7d1455a9905d1870fb963de5ba95a65a4
Your Name <your@email.com>
2026-07-14 16:48:37 +0100

    Add rock paper scissors CLI game

---
+++ lesson1.py
@@ -0,0 +1,12 @@
+print ("jojo")
+print (27)
+name = "jojo"
+age = 27
+city = "abuja"
+print ("My name is", name, "I am", age, "years old and I live in", city)
+height = 6.4
+is_student = True
+print (type(name))
+print (type(age))
+print (type(height))
+print (type(is_student))
